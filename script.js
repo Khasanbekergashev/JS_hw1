04dca754e181f8c16eb13c16b666adf2367df0c4
Khasanbekergashev <72455017+Khasanbekergashev@users.noreply.github.com>
2020-10-23 17:02:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+/* console.log(5);
+//console.log(5);
+console.log("Hi, Steave. How are you? I wanna visit you as soon as possible");
+console.log(true);
+console.log(false);
+console.log(25.54);
+console.log(-999);
+
+var myName = "Hi, Steave. How are you? I wanna visit you as soon as "
+
+console.log(myName);
+
+var name ="Steave"
+var age = 23
+var bvv = true
+var und ;
+console.log(bvv);
+console.log(name);
+console.log(age)
+console.log(und);
+
+
+console.log(8+8);
+console.log(8 + " How are you");
+console.log("I am "+age+" however my name is "+name +". Today the weater is wonderfull. " +bvv +" is always TRUE. Follow me");
+
+var mmm = "I am "+age+" however my name is "+name +". Today the weater is wonderfull. " +bvv +" is always TRUE. Follow me";
+
+console.log(mmm); */
+
+/* 
+console.log(6-3);
+console.log(6+10);
+console.log(6%2);
+console.log("6 + 3 = "+ (6+3));
+console.log("6 + 3 = "+ (6+3));
+console.log("6 - 3 = "+ (6-3)); 
+console.log("5 * 6 = "+ 6*5); */
+
+
+// Модальные окна
+
+// var number = +prompt("Введите дату вашего дня рождения")
+// console.log(number);
+// console.log(+number +2);
+
+// alert(number)
+
+
+var name = prompt("Введите ваше имя");
+console.log('Ваше имя: ' + name);
+
+var age = prompt("Введите ваш возраст")
+console.log("Ваш возраст: " + age + " лет");  
+
+var plus = prompt("Решите пример: 5 + 10 = ?")
+console.log("Пример 1: 5 + 10 = 15; " + "Ваш результат: " + plus);
+
+var minus = prompt("Решите пример: 19 - 16 = ?");
+console.log("Пример 2: 19 - 16 = 3;" + " Ваш результат: " + minus);
+
+var multiple = prompt("Решите пример: 22 * 2 = ? ")
+console.log("Пример 3: 22 * 2 = 44; "+"Ваш результат: "+ multiple);
+
+var division = prompt("Решите пример:  10 : 5 = ?");
+console.log("Пример 4: 10 : 5 = 2;" + " Ваш результат: " + division);
+
+var rest = prompt("Найдите остаток от деления: 22 % 7 = ?")
+console.log("Пример 5: 22 % 7 = 1; " + "Ваш результат: " + rest);
+
+alert("Если хотите проверить ваши ответы, то  откройте вкладку Console в инструменте разработчика, нажав на F12")
